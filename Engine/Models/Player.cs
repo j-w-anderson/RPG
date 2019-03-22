58134c11331230e6f5028fb6fd31f12929600474
James Anderson <james.wanders@gmail.com>
2019-03-21 20:23:32 -0400

    04.3

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Player : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged("Name"); }
+        }
+
+        private string _characterClass;
+
+        public string CharacterClass
+        {
+            get { return _characterClass; }
+            set { _characterClass = value; OnPropertyChanged("CharacterClass"); }
+        }
+        private int _hitPoints;
+
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            set { _hitPoints = value; OnPropertyChanged("HitPoints"); }
+        }
+        private int _experiencePoints;
+
+        public int ExperiencePoints
+        {   
+            get { return _experiencePoints; }
+            set { _experiencePoints = value; OnPropertyChanged("ExperiencePoints"); }
+        }
+
+        private int _level;
+
+        public int Level
+        {
+            get { return _level; }
+            set { _level = value; OnPropertyChanged("Level"); }
+        }
+
+        private int _gold;
+
+        public int Gold
+        {
+            get { return _gold; }
+            set { _gold = value; OnPropertyChanged("Gold"); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,123 @@
+﻿using Engine.Factories;
+using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    public class GameSession:INotifyPropertyChanged
+    {
+        public Player CurrentPlayer { get; set; }
+        private Location _currentLocation;
+
+        public Location CurrentLocation
+        {
+            get { return _currentLocation; }
+            set
+            {
+                _currentLocation = value;
+                OnPropertyChanged("CurrentLocation");
+                OnPropertyChanged("HasLocationToNorth");
+                OnPropertyChanged("HasLocationToEast");
+                OnPropertyChanged("HasLocationToWest");
+                OnPropertyChanged("HasLocationToSouth");
+            }
+        }
+
+        public World CurrentWorld { get; set; }
+
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "James";
+            CurrentPlayer.CharacterClass = "Figher";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+            CurrentPlayer.Gold = 10000;
+            //            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
+
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+            CurrentLocation = CurrentWorld.LocationAt(0, 0);
+        }
+        
+        public void MoveNorth()
+        {
+            int xCoordinate = CurrentLocation.XCoordinate;
+            int yCoordinate = CurrentLocation.YCoordinate;
+            CurrentLocation = CurrentWorld.LocationAt(
+                xCoordinate, 
+                yCoordinate + 1);
+        }
+        public void MoveEast()
+        {
+            int xCoordinate = CurrentLocation.XCoordinate;
+            int yCoordinate = CurrentLocation.YCoordinate;
+            CurrentLocation = CurrentWorld.LocationAt(
+                xCoordinate + 1, 
+                yCoordinate);
+        }
+        public void MoveWest()
+        {
+            int xCoordinate = CurrentLocation.XCoordinate;
+            int yCoordinate = CurrentLocation.YCoordinate;
+            CurrentLocation = CurrentWorld.LocationAt(
+                xCoordinate - 1, 
+                yCoordinate);
+        }
+        public void MoveSouth()
+        {
+            int xCoordinate = CurrentLocation.XCoordinate;
+            int yCoordinate = CurrentLocation.YCoordinate;
+            CurrentLocation = CurrentWorld.LocationAt(
+                xCoordinate,
+                yCoordinate - 1);
+        }
+
+        public bool HasLocationToNorth
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate,
+                    CurrentLocation.YCoordinate + 1) != null;
+            }
+        }
+        public bool HasLocationToEast
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate+1,
+                    CurrentLocation.YCoordinate) != null;
+            }
+        }
+        public bool HasLocationToWest
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate-1,
+                    CurrentLocation.YCoordinate) != null;
+            }
+        }
+        public bool HasLocationToSouth
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate,
+                    CurrentLocation.YCoordinate - 1) != null;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
